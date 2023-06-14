c3dae930597607a713edc3fd7bf9991648f0ff58
mairi tikk <mairi.tikk@gmail.com>
2023-06-14 09:26:37 +0200

    adding #key

--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -1,26 +1,41 @@
 class Coche {
+    #encendido
+    #key
     constructor(color) {
         console.log("me he consruido")
         this.color = color
-        this.encendido = false
+        this.#encendido = false
         this.velocidad = 0
+        this.#key = key
     }
     matricular(matricula){
         this.matricula = matricula
     }
-    arrancar() {
-        this.encendido = true
+    arrancar(key) {
+        if (key !== this.#key) {
+            console.log("falso llave")
+        }else {
+            this.#encendido = true
+        }
+     
     }
-    detener() {
-        this.encendido = false
+    detener(key) {
+        if (key !== this.#key) {
+            console.log("falso llave")
+        }else {
+            this.#encendido = false
+        }
+    }
+    getEncendido(){
+        return this.#encendido
     }
     acelerar(){
-        if (this.encendido){
+        if (this.#encendido){
             this.velocidad++
         }
     }
     frenar() {
-        if (this.encendido && this.velocidad > 1) {
+        if (this.#encendido && this.velocidad > 1) {
             this.velocidad--
         }
     }
@@ -28,7 +43,7 @@ class Coche {
         if(this.velocidad > 0) {
             console.log("no puedes pasar la itv con el coche en moviminento")
         }
-        else if (this.encendido){
+        else if (this.#encendido){
             console.log("apage el coche")
         }
         else{
@@ -37,14 +52,17 @@ class Coche {
         }
     }
 }
-const coche = new Coche("amarillo")
+const key = "123";
+const coche = new Coche("amarillo", key)
 //console.log(coche.color)
 //const otrocoche = new Coche ("rojo")
 //console.log(otrocoche.color)
 //console.log(coche.matricula)
 coche.matricular("123nosdf");
 //console.log(coche.matricula)
-coche.arrancar();
-coche.encendido
-coche.detener();
-coche.encendido = true
+//coche.arrancar();
+//coche.encendido
+//coche.detener();
+// false
+coche.arrancar(key);
+console.log(coche.getEncendido())
