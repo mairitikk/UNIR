45c6e8b650be18a085b12d67c22ac56dd9416004
mairi tikk <mairi.tikk@gmail.com>
2023-06-20 00:13:25 +0200

    add carrito de compra

--- Actividad 4/js/carrito.js
+++ Actividad 4/js/carrito.js
@@ -1,52 +1,77 @@
 class Carrito {
+    #productos
+    #currency
+    #total
+
     constructor(json) {
-        this.currency = json.currency;
-        this.productos = json.products;
+        this.#currency = json.currency;
+        this.#productos = json.products;
 
         //para cada productos agregamos cantidad = 0
-        this.productos.forEach((producto) => {
+        this.#productos.forEach((producto) => {
             producto.cantidad = 0
             producto.total = 0
         })
 
         // calculamos el total
-        const total = this.productos.reduce((acc, producto) =>{
+        this.calcularTotal()
+        
+    }
+
+    calcularTotal(){
+        const total = this.#productos.reduce((acc, producto) =>{
             return acc + producto.total
             
         }, 0)
-        this.total = total
-
+        this.#total = total
     }
 
+    // Actualiza el número de unidades que se 
+    // quieren comprar de un producto (sku)
     actualizarUnidades(sku, unidades) {
-        console.log(this.productos);
-        console.log(this.total)
-        // Actualiza el número de unidades que se quieren comprar de un producto
+        //let producto = this.productos.filter(function (item){ return item.SKU===sku; });
+        let producto = this.#productos.find(item=>item.SKU===sku);       
+        producto.cantidad = unidades
+        producto.total = Number(producto.price) * producto.cantidad
+        this.calcularTotal()
     }
 
+    // Devuelve los datos de un producto 
+    // además de las unidades seleccionadas
     obtenerInformacionProducto(sku) {
-        // Devuelve los datos de un producto además de las unidades seleccionadas
-        // Por ejemplo
-        // {
-        //   "sku": "0K3QOSOV4V",
-        //   "quantity": 3
-        // } 
+        return this.#productos.find(item=>item.SKU===sku);
+    }
+    
+    // Devuelve información de los productos añadidos al carrito
+    // Además del total calculado de todos los productos
+    obtenerCarrito() {
+        let json = {
+            total: this.#total,
+            currency: this.#currency,
+            // 1) products: this.productos,
+            products: [] 
+        };
+        // lo implemento con la función map porque es la mejor solución.
+        // con la solución 1) (ver arriba) cualquier modificación de "products"
+        // me cambia el valor original de "this.productos"
+        this.#productos.map(function(producto){
+            json.products.push({
+                "SKU": producto.SKU,
+                "title": producto.title,
+                "price": producto.price,
+                "cantidad": producto.cantidad,
+                "total": producto.total
+            })
+        })
+        return json
     }
 
-    obtenerCarrito() {
-        // Devuelve información de los productos añadidos al carrito
-        // Además del total calculado de todos los productos
-        // Por ejemplo:
-        // {
-        //   "total": "5820",
-        //   "currency: "€",
-        //   "products" : [
-        //     {
-        //       "sku": "0K3QOSOV4V"
-        //       ..
-        //     }
-        //    ]}
-        // }
+    getCurrency(){
+        return this.#currency
+    }
+
+    getTotal(){
+        return this.#total
     }
 }
 
--- Actividad 4/js/script.js
+++ Actividad 4/js/script.js
@@ -1,107 +1,159 @@
 // Importar la classe carrito
-import Carrito from "./carrito.js"; 
+import Carrito from "./carrito.js";
 
-//Function para llamar a la API
+// Crear función para llamr API
 async function consultarAPI(url) {
   try {
     // consultamos la API
     let response = await fetch(url)
-    
+
     // obtenemos la respuesta como un json
     return await response.json();
-  } catch(error) {
+  } catch (error) {
     console.error('There was a problem fetching the data:', error);
   };
 }
+/*
+() => {
+  // obtener el campo input
 
-let data = await consultarAPI('https://jsonblob.com/api/1117165857469120512')
+  // tomar su valor en entero
+  const inputValueminus = parseInt(producto.cantidad.value);
 
-//declarar una variable carrito
-let carrito = new Carrito(data);
-
-carrito.actualizarUnidades()
-
-const table = document.createElement('table');
-/*const listado = document.querySelector('#listado-alumnos');
-//listado de alumnos
-alumnos.forEach((alumno) => {
-
-    const li = document.createElement('li');//<li></li>
-    li.innerHTML = alumno;//<li>alumno></li>
-    listado.appendChild(li); //<ul> <li>alumno</li> </ul>
-
-});
-*/
-
-
-
-/* // create table element
-var table = document.querySelector('#products')//document.createElement("table");
-
-// create rows and cells using loops
-for (var i = 0; i < 3; i++) {
-  var row = table.insertRow();
-  for (var j = 0; j < 4; j++) {
-    var cell = row.insertCell();
-    cell.innerHTML = "Row " + i + ", Cell " + j;
-  }
-} */
-
-// append table to HTML document
-//document.body.appendChild(table);
-
-/*const data = [
-  {name: 'John Smith', age: 35, occupation: 'Engineer'},
-  {name: 'Jane Doe', age: 28, occupation: 'Designer'},
-  {name: 'Bob Jones', age: 42, occupation: 'Manager'}
-];
-
-
-
-// create a header row
-const headers = ['Name', 'Age', 'Occupation'];
-const headerRow = document.createElement('tr');
-headers.forEach(header => {
-  const th = document.createElement('th');
-  th.textContent = header;
-  headerRow.appendChild(th);
-});
-table.appendChild(headerRow);
-
-// create a row for each data object
-data.forEach(datum => {
-  const row = document.createElement('tr');
-  Object.values(datum).forEach(value => {
-    const cell = document.createElement('td');
-    cell.textContent = value;
-    row.appendChild(cell);
-  });
-  table.appendChild(row);
-});
-
-// add the table to the DOM
-document.body.appendChild(table); */
-
-
-/**
- * 
- * function consultarAPI(url) {
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Network response was not ok');
-      }
-    })
-    .then(data => {
-
-      // Do something with the data, like display it in a table
-      carrito = new Carrito(data);
-
-    })
-    .catch(error => {
-      console.error('There was a problem fetching the data:', error);
-    });
+  // restar la cantidad
+  inputValueminus.valueOf = inputValueminus - 1;
 }
- */
+*/
+const cantidadClickHandler = (event) => {
+  const botton = event.target;
+  const sku = botton.dataset.sku;
+  const signo = botton.innerHTML;
+  actualizarCantidad(sku, signo)
+  actualizarTotal(sku)
+  insertarInfoEnCol2(carrito)
+}
+
+const actualizarTotal = (sku) => {
+  let td = document.querySelector('td[data-sku="' +sku +'"]');
+  td.innerHTML = carrito.obtenerInformacionProducto(sku).total +" "+ carrito.getCurrency()
+}
+
+const actualizarCantidad = (sku, signo) => {
+  let input = document.querySelector('input[data-sku="' + sku + '"]');
+  let value = Number(input.value)
+  if (signo === "-" && value > 0) {
+    input.value = value - 1
+  }
+  if (signo === "+") {
+    input.value = value + 1
+  }
+  carrito.actualizarUnidades(sku, input.value)
+}
+
+
+function insertarInfoEnCol1(carrito) {
+
+  let table = document.querySelector('#products')
+
+  let info = carrito.obtenerCarrito()
+
+  info.products.forEach((product) => {
+
+    let row = table.insertRow();
+    row.classList.add("border-line")
+
+    // columna producto
+    let cell = row.insertCell(); // creamos una columna
+
+    let span = document.createElement("span");
+    span.innerHTML = product.title
+    span.classList.add("heading-2")
+    cell.appendChild(span)
+
+    let p = document.createElement("p")
+    p.innerHTML = "Ref: " + product.SKU
+    p.classList.add("reference")
+    cell.appendChild(p)
+
+
+    // columna cantidad
+    let cell2 = row.insertCell();
+
+    // boton menos
+    let button = document.createElement("button");
+    button.dataset.sku = product.SKU;
+    button.innerHTML = "-";
+    button.classList.add("button-cantidad");
+    cell2.appendChild(button);
+
+    button.addEventListener("click", cantidadClickHandler);
+
+
+    // input
+    const input = document.createElement("input")
+    input.dataset.sku = product.SKU
+    input.type = 'number';
+    input.classList.add("input-number");
+    input.value = "0";
+    input.classList.add("input-number");
+    cell2.appendChild(input);
+
+    // boton mas
+    let button2 = document.createElement("button");
+    button2.dataset.sku = product.SKU;
+    button2.innerHTML = "+";
+    button2.classList.add("button-cantidad");
+    cell2.appendChild(button2);
+
+    button2.addEventListener("click", cantidadClickHandler);
+
+
+    // columna precio
+    let cell3 = row.insertCell();
+    cell3.innerHTML = product.price + " " + carrito.getCurrency();
+
+    // columna total
+    let cell4 = row.insertCell();
+    cell4.dataset.sku = product.SKU;
+    cell4.innerHTML = product.total + " " + carrito.getCurrency();
+
+  })
+}
+
+function insertarInfoEnCol2(carrito){
+
+  let totalProducts = document.getElementById('total__products')
+  totalProducts.innerHTML= " ";
+
+  let info = carrito.obtenerCarrito();
+  info.products.forEach((product) =>{
+    if (product.total != 0 ){
+      let p = document.createElement("p");
+      p.innerHTML = product.title;
+      totalProducts.appendChild(p);
+
+      let p2 = document.createElement("p");
+      p2.innerHTML = product.total;
+      totalProducts.appendChild(p2);
+    }
+  });
+
+  let totalPrice = document.getElementById('total__price')
+  totalPrice.innerHTML= carrito.getTotal() + " " + carrito.getCurrency()
+  console.log(info);
+
+}
+
+
+let json = await consultarAPI('https://jsonblob.com/api/1117165857469120512')
+
+//declarar o instanciar una variable carrito
+let carrito = new Carrito(json);
+
+// carrito.actualizarUnidades('0K3QOSOV4V', 4)
+// console.log(carrito.obtenerInformacionProducto('0K3QOSOV4V'))
+
+insertarInfoEnCol1(carrito)
+
+insertarInfoEnCol2(carrito)
+
