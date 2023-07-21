9564e4b0f1b171fb706a6e4e6bb003e0cce1cb22
mairi tikk <mairi.tikk@gmail.com>
2023-07-21 20:45:35 +0200

    creating formulario y adding list

--- trabajo/segundaApp/src/app/app.component.ts
+++ trabajo/segundaApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Contacto } from './interfaces/contacto.interface';
+import { findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,43 @@ import { Contacto } from './interfaces/contacto.interface';
 })
 export class AppComponent {
   arrContactos: Contacto[] = [];
+  newContacto: Contacto = {name: "", phone: 0};
 
   constructor(){
     this.arrContactos = [
-      {
-        name: 'mairi', phone: 3643290
-      }
+      {name: 'mairi', phone: 3643290},
+        {name: 'maii', phone: 7653643290},
+        {name: 'mairi', phone: 3643290},
+      
     ]
   }
   cargarDatos(): string {
-    return `<li>${this.arrContactos[0].name} - ${this.arrContactos[0].phone} </li>`
+    let lis = "";
+    this.arrContactos.forEach(contacto => {
+      lis += `<li> ${contacto.name} - ${contacto.phone}<li>`
+    })
+    return lis;
+  }
+  guardar(): void{
+
+    // et ei saaks nulliga registeerida
+    if(this.newContacto.name !== "" && (this.newContacto.phone !== 0 || this.newContacto.phone !== null))
+    //antes intsentar tienemos que comprobar si esta en array o no
+    {
+      let busqueda = this.arrContactos.findIndex(contacto =>
+      contacto.name === this.newContacto.name || contacto.phone ===
+      this.newContacto.phone)
+      if(busqueda === -1){
+        //no existe inseto el elemento en el array
+    this.arrContactos.push(this.newContacto);
+    this.newContacto = {name:"", phone: 0} }
+    else {
+      //existe lanzo el masaje duuplicado
+      alert('this contact is dublicate')
+    }
+  }
+    else{alert('los campos no pueden ser vacios')
+  }
   }
 }
+
--- trabajo/segundaApp/src/app/app.module.ts
+++ trabajo/segundaApp/src/app/app.module.ts
@@ -1,14 +1,18 @@
+//liberia
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+//components
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
