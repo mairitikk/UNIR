94d96f8ace5a3b543a334212903d3bc06e2029fb
mairi tikk <mairi.tikk@gmail.com>
2023-07-29 21:29:48 +0200

    cambiar stylos forma dynamica

--- trabajo/directivas/src/app/components/ng-style/ng-style.component.ts
+++ trabajo/directivas/src/app/components/ng-style/ng-style.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class NgStyleComponent {
 fontSize: number = 50;
 color: string = 'yellow';
+
+cambiarFuente($event: any){
+  this.fontSize = $event.target.value;
+}
+cambiarColor($event: any){
+  this.color = $event.target.value;
+}
 }
