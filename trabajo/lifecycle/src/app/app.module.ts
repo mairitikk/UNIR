2a9081435f03792a33610babd5eea395ea98d1ba
mairi tikk <mairi.tikk@gmail.com>
2023-08-01 22:52:00 +0200

    eliminando componente

--- trabajo/lifecycle/src/app/app.module.ts
+++ trabajo/lifecycle/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UnoComponent } from './components/uno/uno.component';
-import { DosComponent } from './components/dos/dos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UnoComponent,
-    DosComponent,
+  
   ],
   imports: [
     BrowserModule
