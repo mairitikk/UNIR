12cb97940f680d2a68a39ef12049662f4c8d4f9d
mairi tikk <mairi.tikk@gmail.com>
2023-08-21 21:33:16 +0200

    valiador dni y edad

--- trabajo/formularios/src/app/components/form-model/form-model.component.ts
+++ trabajo/formularios/src/app/components/form-model/form-model.component.ts
@@ -25,15 +25,68 @@ export class FormModelComponent {
     edad: new FormControl('', [
       this.edadValidator
     ]),
-    dni: new FormControl('', []),
+    dni: new FormControl('', [
+      this.dniValidator
+    ]),
     password: new FormControl('', []),
     repeatpassword: new FormControl('', [])
   }, []);
 }
+dniValidator(controlName: AbstractControl): any {
 
-edadValidator(controlName: AbstractControl){
+  //validar que dni esta bien escrito 8 numeros y un letra en mayusculas. Lo voy a comprobar con una expresion regular.
+
+  const letrasDni: string[] = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
+
+  const dni = controlName.value;
+
+  const exp = /^\d{8}[A-Z]{1}/
+
+
+
+  if (exp.test(dni)) {
+
+    //el dni esta bien y solo hay que comprobar la letra
+
+    const letra: string = dni.at(-1);
+
+    const numero: number = parseInt(dni.substring(0, dni.length - 1))
+
+
+
+    const position: number = numero % 23;
+
+
+
+    return (letra !== letrasDni[position]) ? { 'dnivalidator': 'La letra no corresponde con el dni' } : null
+
+
+
+  } else {
+
+    return { 'dnivalidator': 'formato del dni incorrecto' }
+
+  }
+
+
+
+  //Un dni es valido si divido solo la parte numerica del dni por 23 y el resto de esa división es la posicion de una letra dentro de un array. 
+
+
+
+}
+
+edadValidator(controlName: AbstractControl): any {
   //podemos hacer la logica qie ceramos, un rango de edad º
-return {}
+
+  const edad: number = parseInt(controlName.value);
+  if(isNaN(edad)){
+return {'edadvalidator': 'No es numero'}
+  }else if(edad < 18 || edad > 65){
+    return{ 'edadvalidator': 'rango de edad 18 y 65'}
+
+  }
+  return null
 }
 getDataForm(){
   console.log(this.formModel.value);
